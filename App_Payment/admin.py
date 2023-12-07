3474090b80ea1c59a4e875601298bd48e3f7b1be
Alkuma Akther Munna <117329055+AlkumaMunna@users.noreply.github.com>
2023-12-06 22:00:01 -0500

    Delete App_Payment directory

--- App_Payment/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from App_Payment.models import BillingAddress, Coupon_Code
-
-# Register your models here.
-admin.site.register(BillingAddress)
-admin.site.register(Coupon_Code)
